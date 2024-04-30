9e50c73c9edf74bd932dad699fc3ca5cb81318ef
Nicholas Pease <me@nicholaspease.com>
2024-04-30 18:50:07 +0000

    Change Redirects

--- pease-portfolio/src/app/previous_versions/legacy/page.js
+++ pease-portfolio/src/app/previous_versions/legacy/page.js
@@ -1,5 +1,5 @@
 import { redirect } from 'next/navigation';
 export default function Home() {
-    redirect("legacy/index.html")
+    redirect("/index.html")
     return <div className="pl-6">Redirecting to legacy... <br/><br/> Please Wait</div>
 }
