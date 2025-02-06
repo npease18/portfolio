a416f1843c6f718658577dbf6a6042c9ecc17f0e
Nicholas Pease <me@nicholaspease.com>
2025-02-05 19:33:24 -0500

    Add Speed Insights

--- pease-portfolio/src/app/layout.js
+++ pease-portfolio/src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import "./globals.css";
 
 import dynamic from 'next/dynamic'
@@ -23,6 +24,7 @@ export default function RootLayout({ children }) {
         <Navigation/>
         {children}
         <Analytics />
+        <SpeedInsights />
         <Footer/>
       </body>
     </html>
