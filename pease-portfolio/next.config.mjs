268ce906bbc58a039fe425ce9e34957d74eaeeee
Nicholas Pease <me@nicholaspease.com>
2025-12-02 19:52:32 -0500

    feat: add skydive picture

--- pease-portfolio/next.config.mjs
+++ pease-portfolio/next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    async rewrites() {
+      return [
+        {
+          source: '/previous_versions/legacy',
+          destination: '/previous_versions/legacy/index.html',
+        },
+      ]
+    },
+  }
+ 
+export default nextConfig
