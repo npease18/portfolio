6e9c37914f11cc48716c7b5cd14c29f595ef701d
Nicholas Pease <me@nicholaspease.com>
2026-01-17 21:58:58 -0500

    refactor: cleanup

--- pease-portfolio/src/app/about-me/page.js
+++
@@ -1,8 +0,0 @@
-export default function Page(){
-
-    return (
-        <div className="pl-[2%] w-[96%]">
-            <span className="text-[40px] font-bold">About Me</span>
-        </div>
-    )
-}
